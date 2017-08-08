883d219a718fb4681100f03531a1a0dfb7f243ad
hungmuha <hung.soboss@gmail.com>
2017-08-07 23:09:58 -0600

    getting heroku ready to deploy

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/project-2');
+mongoose.connect( process.env.MONGODB_URI ||'mongodb://localhost/project-2');
 
 module.exports.Stock= require("./stock");
 module.exports.User= require("./user");
